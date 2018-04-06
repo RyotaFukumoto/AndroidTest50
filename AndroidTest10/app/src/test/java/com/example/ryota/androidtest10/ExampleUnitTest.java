4980af7b7456e301be6164dd1f04adc109caec1f
RyotaFukumoto <k014c1106@it-neec.jp>
2018-04-06 13:58:23 +0900

    AndroidTest10 complete

---
+++ AndroidTest10/app/src/test/java/com/example/ryota/androidtest10/ExampleUnitTest.java
@@ -0,0 +1,10 @@
+package com.example.ryota.androidtest10;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+class ExampleUnitTest {
+
+}
