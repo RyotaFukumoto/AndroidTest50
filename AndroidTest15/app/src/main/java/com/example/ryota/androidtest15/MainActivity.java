8255b064147f7b617404677dad6360ea0e6246b8
RyotaFukumoto <k014c1106@it-neec.jp>
2018-04-09 16:44:38 +0900

    AndroidTest15 complete

--- AndroidTest15/app/src/main/java/com/example/ryota/androidtest15/MainActivity.java
+++ AndroidTest15/app/src/main/java/com/example/ryota/androidtest15/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.ryota.androidtest15;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -19,25 +21,33 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new AlertDialog.Builder(MainActivity.this)
-                        .setTitle("title")
-                        .setMessage("message")
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // OK button pressed
-                                Log.i("AlertDialog1","OK");
-                            }
-                        })
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // OK button pressed
-                                Log.i("AlertDialog2","cancel");
-                            }
-                        })
-                        .show();
+                DialogFragment fragment = new TestDialogFragment();
+                fragment.show(getFragmentManager(),"test");
             }
         });
+    }
+    public static class TestDialogFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage("テストダイアログ")
+            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // OK button pressed
+                    Log.i("AlertDialog1","ok");
+                }
+            })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // OK button pressed
+                            Log.i("AlertDialog2","cancel");
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
     }
 }
