eb027e871d4522d0c2a494ef45d5f0a723534bb9
RyotaFukumoto <k014c1106@it-neec.jp>
2018-04-09 14:19:41 +0900

    AndroidTest13 complete

---
+++ AndroidTest13/app/src/test/java/com/example/ryota/androidtest13/ExampleUnitTest.java
@@ -0,0 +1,10 @@
+package com.example.ryota.androidtest13;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+class ExampleUnitTest {
+
+}
