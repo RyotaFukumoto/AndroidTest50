c93d50259662d875973d035d121cf53f06070cf9
RyotaFukumoto <k014c1106@it-neec.jp>
2018-04-12 14:04:31 +0900

    AndroidTest15 complete

--- AndroidTest15/app/src/main/java/com/example/ryota/androidtest15/MainActivity.java
+++ AndroidTest15/app/src/main/java/com/example/ryota/androidtest15/MainActivity.java
@@ -1,12 +1,8 @@
 package com.example.ryota.androidtest15;
 
-import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -22,32 +18,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 DialogFragment fragment = new TestDialogFragment();
-                fragment.show(getFragmentManager(),"test");
+                fragment.show(getFragmentManager(), "test");
             }
         });
-    }
-    public static class TestDialogFragment extends DialogFragment {
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the Builder class for convenient dialog construction
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setMessage("テストダイアログ")
-            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    // OK button pressed
-                    Log.i("AlertDialog1","ok");
-                }
-            })
-                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            // OK button pressed
-                            Log.i("AlertDialog2","cancel");
-                        }
-                    });
-            // Create the AlertDialog object and return it
-            return builder.create();
-        }
     }
 }
