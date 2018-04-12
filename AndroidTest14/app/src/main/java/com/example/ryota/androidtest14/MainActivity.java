d85813662994c4a65339dde929292b63740f85be
RyotaFukumoto <k014c1106@it-neec.jp>
2018-04-12 14:02:37 +0900

    AndroidTest14 complete

--- AndroidTest14/app/src/main/java/com/example/ryota/androidtest14/MainActivity.java
+++ AndroidTest14/app/src/main/java/com/example/ryota/androidtest14/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.ryota.androidtest14;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ImportFlag.onClickListener {
 
+    Output output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        output = (Output) getSupportFragmentManager().findFragmentById(R.id.outputFragment);
+
+    }
+
+    @Override
+    public void onClick(String text) {
+        output.setTextView(text);
     }
 }
--- AndroidTest14/app/src/main/java/com/example/ryota/androidtest14/Output.java
+++ AndroidTest14/app/src/main/java/com/example/ryota/androidtest14/Output.java
@@ -6,16 +6,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Output extends Fragment{
-    private TextView textView;
-
+    TextView textView;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.output, container, false);
+        View view = inflater.inflate(R.layout.output, container, false);
+        this.textView = view.findViewById(R.id.textView);
+        return view;
+    }
+
+
+    public void setTextView(String text) {
+        this.textView.setText(text);
     }
 
 }
