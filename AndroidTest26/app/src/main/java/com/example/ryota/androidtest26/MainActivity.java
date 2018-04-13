d4c754e633fa3de0b684b295df151ac06be8271a
RyotaFukumoto <k014c1106@it-neec.jp>
2018-04-13 09:23:35 +0900

    AndroidTest25 complete

--- AndroidTest26/app/src/main/java/com/example/ryota/androidtest26/MainActivity.java
+++ AndroidTest26/app/src/main/java/com/example/ryota/androidtest26/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.ryota.androidtest26;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseHelper databaseHelper = new DatabaseHelper(getApplicationContext());
+        databaseHelper.getWritableDatabase();
+
+        FloatingActionButton button = findViewById(R.id.floatingActionButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(),NewToDoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
