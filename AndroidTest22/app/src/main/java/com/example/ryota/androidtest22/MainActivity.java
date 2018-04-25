121481c7f0a8dac4b112860a9e08d595a322cd6c
RyotaFukumoto <k014c1106@it-neec.jp>
2018-04-25 14:47:02 +0900

    レビューをしていただいた点を修正しましたので、再提出します。

--- AndroidTest22/app/src/main/java/com/example/ryota/androidtest22/MainActivity.java
+++ AndroidTest22/app/src/main/java/com/example/ryota/androidtest22/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.ryota.androidtest22;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
--- AndroidTest22/app/src/main/java/com/example/ryota/androidtest22/SubActivity.java
+++ AndroidTest22/app/src/main/java/com/example/ryota/androidtest22/SubActivity.java
@@ -1,14 +1,10 @@
 package com.example.ryota.androidtest22;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-@SuppressLint("Registered")
-@SuppressWarnings("UnnecessarilyQualifiedInnerClassAccess")
 public class SubActivity extends AppCompatActivity {
 
     @Override
