78df7d94830a441c07488caedb9212b1cdcf0abf
RyotaFukumoto <k014c1106@it-neec.jp>
2018-04-30 20:56:39 +0900

    レビューをしていただいた点を修正しましたので、再提出します。

--- AndroidTest11/app/src/main/java/com/example/ryota/androidtest11/MainActivity.java
+++ AndroidTest11/app/src/main/java/com/example/ryota/androidtest11/MainActivity.java
@@ -14,30 +14,30 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
 
     private InputMethodManager inputMethodManager;
     private ConstraintLayout mainLayout;
     private EditText editText;
     private String text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.mainLayout = (ConstraintLayout) findViewById(R.id.layout);
 
-
+        this.inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         setContentView(R.layout.activity_main);
-        setInputMethodManager((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE));
         editText = findViewById(R.id.editText);
-        this.mainLayout = (ConstraintLayout) findViewById(R.id.main_layout);
-        
+
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
 
                 if (event.getAction() == KeyEvent.ACTION_DOWN
-                        && keyCode == KeyEvent.KEYCODE_ENTER ) {
-                    if(editText.length() > 0) {
+                        && keyCode == KeyEvent.KEYCODE_ENTER) {
+                    if (editText.length() > 0) {
 
                         inputMethodManager.hideSoftInputFromWindow(editText.getWindowToken(),
                                 InputMethodManager.RESULT_SHOWN);
@@ -58,26 +58,12 @@ public class MainActivity extends AppCompatActivity  {
         return true;
     }
 
-    private void hideKeyboard(){
-        // キーボードを隠す
+    private void hideKeyboard() {
 
         this.inputMethodManager.hideSoftInputFromWindow(this.mainLayout.getWindowToken(),
                 InputMethodManager.HIDE_NOT_ALWAYS);
-// 背景にフォーカスを移す
         this.mainLayout.requestFocus();
 
     }
 
-
-
-
-
-
-    public InputMethodManager getInputMethodManager() {
-        return this.inputMethodManager;
-    }
-
-    public void setInputMethodManager(InputMethodManager inputMethodManager) {
-        this.inputMethodManager = inputMethodManager;
-    }
 }
