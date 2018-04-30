ad381930b12aa642ec4be88d64719cc6f6b92aca
RyotaFukumoto <k014c1106@it-neec.jp>
2018-04-30 17:53:48 +0900

    レビューをしていただいた点を修正しましたので、再提出します。

--- AndroidTest45/app/src/main/java/com/example/ryota/androidtest45/MainActivity.java
+++ AndroidTest45/app/src/main/java/com/example/ryota/androidtest45/MainActivity.java
@@ -1,20 +1,21 @@
 package com.example.ryota.androidtest45;
 
-import android.Manifest;
-import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.TextView;
+import android.content.Intent;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
+import android.Manifest;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
 
