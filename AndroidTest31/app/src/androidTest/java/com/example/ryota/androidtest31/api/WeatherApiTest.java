d4b87da09f374d493fa9511b5a1c2fa74857d8f9
RyotaFukumoto <k014c1106@it-neec.jp>
2018-05-07 05:58:14 +0900

    AndroidTest34 complete

---
+++ AndroidTest31/app/src/androidTest/java/com/example/ryota/androidtest31/api/WeatherApiTest.java
@@ -0,0 +1,37 @@
+package com.example.ryota.androidtest31.api;
+
+import com.example.ryota.androidtest31.api.model.Weather;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+public class WeatherApiTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getWeather() {
+        WeatherApi weatherApi =  new WeatherApi(new WeatherApi.WeatherApiCallback() {
+            @Override
+            public void success(Weather weather) {
+                assertEquals(weather.getLocation().getCity(),"東京");
+                assertEquals(weather.getForecasts().get(0).getDateLabel(),"今日");
+            }
+
+            @Override
+            public void failed() {
+
+            }
+        });
+        weatherApi.getWeather();
+    }
+}
