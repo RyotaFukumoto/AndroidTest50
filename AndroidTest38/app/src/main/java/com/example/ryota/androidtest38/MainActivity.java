f4a76dd9ad596a383b6a14fe77b8c6915adcf515
RyotaFukumoto <k014c1106@it-neec.jp>
2018-05-06 11:46:36 +0900

    レビューをしていただいた点を修正しましたので、再提出します。

--- AndroidTest38/app/src/main/java/com/example/ryota/androidtest38/MainActivity.java
+++ AndroidTest38/app/src/main/java/com/example/ryota/androidtest38/MainActivity.java
@@ -1,31 +1,46 @@
 package com.example.ryota.androidtest38;
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
+
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 105;
     private static final int RESULT_CAMERA = 1001;
     private ImageView imageView;
-
     private Uri cameraUri;
-
+    private boolean permission = false;
 
 
     @Override
@@ -34,86 +49,147 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        this.imageView = (ImageView) findViewById(R.id.imageView);
+        checkPermission();
+        imageView = (ImageView) findViewById(R.id.imageView);
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(checkPermissionREAD_EXTERNAL_STORAGE(getApplication())) {
-                    String filename = System.currentTimeMillis() + ".jpg";
-                    ContentValues cValues = new ContentValues();
-                    cValues.put(MediaStore.MediaColumns.TITLE, filename);
-                    cValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg");
-                    MainActivity.this.cameraUri = getContentResolver().insert(
-                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, cValues);
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT, MainActivity.this.cameraUri);
-                    startActivityForResult(intent, RESULT_CAMERA);
+                checkPermission();
+                if (permission) {
+                    openCamera();
                 }
             }
         });
     }
 
+    private void openCamera() {
+        String filename = System.currentTimeMillis() + ".jpg";
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, filename);
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        cameraUri = getContentResolver().insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, cameraUri);
+        startActivityForResult(intent, RESULT_CAMERA);
+    }
+
+    private String getFilePath(Uri uri) {
+        String[] projection = {MediaStore.MediaColumns.DATA};
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if (cursor != null) {
+            String path = null;
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(0);
+            }
+            cursor.close();
+            if (path != null) {
+                return path;
+            }
+        }
+        return null;
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == RESULT_CAMERA) {
-            this.imageView.setImageURI(this.cameraUri);
+            try {
+                Bitmap image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), cameraUri);
+                int width = image.getWidth();
+                int height = image.getHeight();
+                Matrix mat = new Matrix();
+                mat.postRotate(90);
+                String photoPath = getFilePath(cameraUri);
+                if (photoPath == null) {
+                    return;
+                }
+                ExifInterface ei = new ExifInterface(photoPath);
+                int orientation = ei.getAttributeInt(ExifInterface.TAG_APERTURE_VALUE,
+                        ExifInterface.ORIENTATION_UNDEFINED);
+                Bitmap bitmap = Bitmap.createBitmap(image, 0, 0, width, height, null, false);
+                Bitmap rotatedBitmap = null;
+                switch (orientation) {
+
+                    case ExifInterface.ORIENTATION_ROTATE_90:
+                        rotatedBitmap = rotateImage(bitmap, 90);
+                        break;
+
+                    case ExifInterface.ORIENTATION_ROTATE_180:
+                        rotatedBitmap = rotateImage(bitmap, 180);
+                        break;
+
+                    case ExifInterface.ORIENTATION_ROTATE_270:
+                        rotatedBitmap = rotateImage(bitmap, 270);
+                        break;
+
+                    case ExifInterface.ORIENTATION_NORMAL:
+                    default:
+                        rotatedBitmap = bitmap;
+                }
+                imageView.setImageBitmap(rotatedBitmap);
+            } catch (IOException e) {
+                Log.e("err", e.getMessage());
+            }
         }
     }
 
-    private boolean checkPermissionREAD_EXTERNAL_STORAGE(
-            Context context) {
-        int currentAPIVersion = Build.VERSION.SDK_INT;
-        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-                return true;
-            } else {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(
-                        this,
-                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                    showDialog(this
-                    );
-
-                } else {
-                    ActivityCompat
-                            .requestPermissions(
-                                    (Activity) this,
-                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-                }
-                return false;
+    public void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+            permission = true;
+        }
+        else {
+            requestPermission();
+        }
+    }
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
             }
 
         } else {
-            return true;
+            Toast toast = Toast.makeText(this,
+                    "許可されていません", Toast.LENGTH_SHORT);
+            toast.show();
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+            }
+
         }
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+                                           String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    permission = true;
+                    openCamera();
+                } else {
+                }
                 break;
             default:
                 super.onRequestPermissionsResult(requestCode, permissions,
                         grantResults);
         }
     }
-    private void showDialog(final Context context) {
-        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
-        alertBuilder.setCancelable(true);
-        alertBuilder.setTitle("Permission necessary");
-        alertBuilder.setMessage("External storage" + " permission is necessary");
-        alertBuilder.setPositiveButton(android.R.string.yes,
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        ActivityCompat.requestPermissions((Activity) context,
-                                new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
-                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-                    }
-                });
-        AlertDialog alert = alertBuilder.create();
-        alert.show();
+
+    private Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
     }
 }
